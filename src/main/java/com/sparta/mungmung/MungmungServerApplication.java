c6d4f020bfdb84bc6e3c931cf22836a756f6cfea
wonhee77 <leehi402@naver.com>
2021-07-15 20:43:58 +0900

    local time 적용 및 CORS domain 추가

--- src/main/java/com/sparta/mungmung/MungmungServerApplication.java
+++ src/main/java/com/sparta/mungmung/MungmungServerApplication.java
@@ -5,19 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class MungmungServerApplication {
 
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("현재 시간 = " + new Date());
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(MungmungServerApplication.class, args);
     }
 
-    @PostConstruct
-    public void started(){
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
+
 
 }
